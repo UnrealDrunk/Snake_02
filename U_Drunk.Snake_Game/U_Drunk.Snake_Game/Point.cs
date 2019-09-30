4d80ddccbe0841dab59f74e3b9ca25e5dcc30e0a
Vladimir_Kozhevnikov <vs.kozh@list.ru>
2019-09-30 23:10:42 +0300

    Encapsulation

--- U_Drunk.Snake_Game/U_Drunk.Snake_Game/Point.cs
+++ U_Drunk.Snake_Game/U_Drunk.Snake_Game/Point.cs
@@ -11,6 +11,23 @@ namespace U_Drunk.Snake_Game
         public int x, y;
         public char sym;
 
+        public Point()
+        {
+            
+        }
+        /// <summary>
+        /// Конструктор по двум точкам с обозначением символа точки
+        /// </summary>
+        /// <param name="_x">координата по оси х</param>
+        /// <param name="_y">координата по оси у</param>
+        /// <param name="_sym">внешний вид символа</param>
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
 
         public void Draw()
         {
