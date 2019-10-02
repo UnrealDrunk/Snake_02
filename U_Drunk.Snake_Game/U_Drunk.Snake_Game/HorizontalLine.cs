d95ae12cc2cdee107f15a5c57782645001bdd47e
Vladimir_Kozhevnikov <vs.kozh@list.ru>
2019-10-02 22:46:33 +0300

    Horizontal Line

---
+++ U_Drunk.Snake_Game/U_Drunk.Snake_Game/HorizontalLine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U_Drunk.Snake_Game
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char sym )
+        {
+            pList = new List<Point>();
+
+            for(int x = xLeft; x<= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+
+
+        }
+
+        public void Drow()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+
+
+    }
+}
--- U_Drunk.Snake_Game/U_Drunk.Snake_Game/Program.cs
+++ U_Drunk.Snake_Game/U_Drunk.Snake_Game/Program.cs
@@ -21,24 +21,8 @@ namespace U_Drunk.Snake_Game
 
             List<int> numList = new List<int>();
 
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine line = new HorizontalLine(5,10,8,'+');
+            line.Drow();
 
 
 
