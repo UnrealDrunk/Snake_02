80df26b7ec6ead3cc73a07a9c71d1b555efa50d7
Vladimir_Kozhevnikov <vs.kozh@list.ru>
2019-10-02 22:23:17 +0300

    List & Homework

--- U_Drunk.Snake_Game/U_Drunk.Snake_Game/Program.cs
+++ U_Drunk.Snake_Game/U_Drunk.Snake_Game/Program.cs
@@ -13,11 +13,14 @@ namespace U_Drunk.Snake_Game
             Point p1 = new Point(1, 3,'*');
        
 
-            p1.Draw();
+            //p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
 
-            p2.Draw();
+            //p2.Draw();
+
+            Point p3 = new Point(6, 7, '%');
+            Point p4 = new Point(5, 12, '$');
 
             List<int> numList = new List<int>();
 
@@ -29,16 +32,47 @@ namespace U_Drunk.Snake_Game
             int y = numList[1];
             int z = numList[2];
 
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
+            //foreach (int i in numList)
+            //{
+            //    Console.WriteLine(i);
+            //}
 
             numList.RemoveAt(0);
 
             List<Point> pList = new List<Point>();
             pList.Add(p1);
             pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+
+            char sym1 = '*';
+            char sym2 = '#';
+            char sym3 = '&';
+            char sym4 = '%';
+
+            List<char> cList = new List<char>();
+
+            cList.Add(sym1);
+            cList.Add(sym2);
+            cList.Add(sym3);
+            cList.Add(sym4);
+
+            //foreach(char c in cList)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+
+
+
+
+
+
 
 
 
